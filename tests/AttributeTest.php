ac1e5e84afba718c09a657b152b122239c0bb842
miladev-ent <milwad.dev@gmail.com>
2023-04-05 16:44:36 +0330

    add `test product has attribute value`

--- tests/AttributeTest.php
+++ tests/AttributeTest.php
@@ -7,6 +7,7 @@ use Milwad\LaravelAttributes\Tests\SetUp\Models\Product;
 
 use function Pest\Laravel\assertDatabaseCount;
 use function PHPUnit\Framework\assertEmpty;
+use function PHPUnit\Framework\assertNotEmpty;
 
 uses(RefreshDatabase::class);
 
@@ -38,4 +39,12 @@ test('test attributes can retrieve in model relation', function () {
     $product = Product::query()->with('attributes')->first();
 
     assertEmpty($product->attributes()->get());
+});
+
+test('test product has attribute value', function () {
+    $product = Product::query()->create(['title' => 'milwad-dev']);
+    $product->attachAttribute('role', $value = 'developer');
+
+    assertDatabaseCount('products', 1);
+    assertNotEmpty($product->hasAttributeValue($value));
 });
