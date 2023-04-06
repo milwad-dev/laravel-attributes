343b3ee82d7763ee25ea33dd7a2959742972fd1f
miladev-ent <milwad.dev@gmail.com>
2023-04-06 11:37:36 +0330

    add property into Attribute model

--- src/Attribute.php
+++ src/Attribute.php
@@ -4,6 +4,12 @@ namespace Milwad\LaravelAttributes;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $title
+ * @property string $value
+ * @property string $attributable
+ * @property string|int $attributable_id
+ */
 class Attribute extends Model
 {
     protected $guarded = [];
